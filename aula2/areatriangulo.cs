0ac28ae4b577edab8e985ed913f4ca604a5af5cb
josevilanir <104863215+josevilanir@users.noreply.github.com>
2022-10-05 10:27:12 -0300

    Create areatriangulo.cs

---
+++ aula2/areatriangulo.cs
@@ -0,0 +1,13 @@
+using system;
+
+class MainClass{
+  public static void Main (string[] args) {
+   double b, h,a;
+   Console.writeLine("Digite a base do triangulo");
+   b = double.parse(conse.readline());
+   console.writeLine("digite a altura do triangulo");
+   h =  double.parse(Console.readLine()_;
+   a = b*h/2;
+   console.writeline($"Área = {a:0.00}");
+  }
+}
